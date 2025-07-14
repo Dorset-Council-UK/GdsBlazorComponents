79d671f0ea4b72758025d0c084951a98e3b4a330
weolo <matt.mailuk@gmail.com>
2025-07-14 15:18:41 +0100

    New GDS Input Number component which supports int and float numbers

---
+++ GdsBlazorComponents/GdsInputNumber.razor.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Components;
+using System.Globalization;
+
+namespace GdsBlazorComponents;
+
+public partial class GdsInputNumber
+{
+    [CascadingParameter]
+    private string? CascadedId { get; set; }
+
+    [Parameter, EditorRequired]
+    public string Text { get; set; } = "";
+
+    [Parameter]
+    public EventCallback<string> TextChanged { get; set; }
+
+    [Parameter]
+    public int? WholeNumber { get; set; }
+
+    [Parameter]
+    public EventCallback<int?> WholeNumberChanged { get; set; }
+
+    [Parameter]
+    public float? FloatNumber { get; set; }
+
+    [Parameter]
+    public EventCallback<float?> FloatNumberChanged { get; set; }
+
+    [Parameter]
+    public bool IsWholeNumber { get; set; } = true;
+
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public bool Show { get; set; } = true;
+
+    [Parameter]
+    public InputNumberWidths Width { get; set; } = InputNumberWidths.None;
+
+    private string? _inputmode;
+    private string? _describedBy;
+    private string? _cssClass;
+
+    protected override void OnInitialized()
+    {
+        Id ??= CascadedId;
+
+        if (IsWholeNumber)
+        {
+            _inputmode = "numeric";
+        }
+
+        _describedBy = $"{Id}-hint {Id}-error";
+
+        _cssClass = (Width == InputNumberWidths.None) ? "govuk-input" : $"govuk-input govuk-input--width-{(uint)Width}";
+    }
+
+    private async Task AfterSetText()
+    {
+        if (int.TryParse(Text, CultureInfo.InvariantCulture, out var wholeNumber))
+        {
+            WholeNumber = wholeNumber;
+        }
+        else
+        {
+            WholeNumber = null;
+        }
+
+        if (float.TryParse(Text, CultureInfo.InvariantCulture, out var floatNumber))
+        {
+            FloatNumber = floatNumber;
+        }
+        else
+        {
+            FloatNumber = null;
+        }
+
+        await TextChanged.InvokeAsync(Text);
+        await WholeNumberChanged.InvokeAsync(WholeNumber);
+        await FloatNumberChanged.InvokeAsync(FloatNumber);
+    }
+}
---
+++ GdsBlazorComponents/InputNumberWidths.cs
@@ -0,0 +1,11 @@
+﻿namespace GdsBlazorComponents;
+
+public enum InputNumberWidths
+{
+    None = 0,
+    Three = 3,
+    Four = 4,
+    Five = 5,
+    Ten = 10,
+    Twenty = 20,
+}
