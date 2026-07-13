033e84125f1f8f29150d76c7e57db08e340f4938
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-07-13 13:53:42 +0100

    New form group context which responds to value changes and state changes

---
+++ GdsBlazorComponents/GdsFormGroupContext.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Components.Forms;
+
+namespace GdsBlazorComponents;
+
+public record GdsFormGroupContext(Action OnChange)
+{
+    public string? Id {
+        get;
+        set
+        {
+            string? trimmed = value?.Trim();
+            if (field == trimmed)
+            {
+                return;
+            }
+            field = trimmed;
+            OnChange.Invoke();
+        }
+    }
+
+    public string? HintId
+    {
+        get;
+        set
+        {
+            string? trimmed = value?.Trim();
+            if (field == trimmed)
+            {
+                return;
+            }
+            field = trimmed;
+            OnChange.Invoke();
+        }
+    }
+
+    public string? ErrorId {
+        get;
+        set
+        {
+            string? trimmed = value?.Trim();
+            if (field == trimmed)
+            {
+                return;
+            }
+            field = trimmed;
+            OnChange.Invoke();
+        }
+    }
+
+    public string? DescribedBy
+    {
+        get
+        {
+            if (HintId is null && ErrorId is null)
+            {
+                return null;
+            }
+            return $"{HintId} {ErrorId}".Trim();
+        }
+    }
+
+    public FieldIdentifier? FieldIdentifier
+    {
+        get;
+        set
+        {
+            if (field.Equals(value))
+            {
+                return;
+            }
+            field = value;
+            OnChange.Invoke();
+        }
+    }
+}
