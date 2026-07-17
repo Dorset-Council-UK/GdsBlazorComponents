d9783f730aa895f1f32ef4e3bcd5f2a03484fc0d
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-07-17 16:54:06 +0100

    New divider base abstract component

---
+++ GdsBlazorComponents/GdsDividerBase.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GdsBlazorComponents;
+
+public abstract class GdsDividerBase : ComponentBase
+{
+    [Parameter]
+    public string Text { get; set; } = "or";
+
+    [Parameter]
+    public string? AdditionalCssClasses { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    protected abstract string BaseCssClass { get; }
+
+    protected string? CssClass => new CssClassBuilder(BaseCssClass)
+        .Add(AdditionalCssClasses)
+        .Build();
+}
