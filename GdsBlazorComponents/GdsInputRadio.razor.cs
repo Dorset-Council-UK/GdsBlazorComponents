445c498553d1ac0f517586b9c1e577de9c6cacd4
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-07-20 15:53:57 +0100

    new GDS input radio component wrapping Blazor's InputRadio

---
+++ GdsBlazorComponents/GdsInputRadio.razor.cs
@@ -0,0 +1,67 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GdsBlazorComponents;
+
+public partial class GdsInputRadio<TValue>
+{
+    [CascadingParameter]
+    private FieldContext? CascadedFieldContext { get; set; }
+
+    [CascadingParameter(Name = "DefaultName")]
+    private string? DefaultName { get; set; }
+
+    [Parameter]
+    public string? Id { get; set; }
+
+    [Parameter]
+    public string? ConditionalId { get; set; }
+
+    [Parameter]
+    public string? AdditionalCssClasses { get; set; }
+
+    private string? _resolvedId;
+    private string? _class;
+
+    protected override void OnParametersSet()
+    {
+        base.OnParametersSet();
+
+        _class = new CssClassBuilder("govuk-radios__input")
+            .Add(AdditionalCssClasses)
+            .Build();
+
+        _resolvedId = CalculateId();
+
+        // update the field context
+        if (CascadedFieldContext is not null)
+        {
+            CascadedFieldContext.InputId = _resolvedId;
+            CascadedFieldContext.NotifyIfChanged();
+        }
+    }
+
+    private string? CalculateId()
+    {
+        // Calculate the id
+        if (!string.IsNullOrWhiteSpace(Id))
+        {
+            // if id is set, use it
+            return Id.Trim();
+        }
+
+        if (!string.IsNullOrWhiteSpace(Name))
+        {
+            // generate a default id using name and value
+            return $"{Name.ToLowerInvariant()}-{Value?.ToString()?.ToLowerInvariant()}";
+        }
+
+        if (!string.IsNullOrWhiteSpace(DefaultName))
+        {
+            // generate a default id using default name and value
+            return $"{DefaultName.ToLowerInvariant()}-{Value?.ToString()?.ToLowerInvariant()}";
+        }
+
+        // use the existing id
+        return CascadedFieldContext?.InputId;
+    }
+}
