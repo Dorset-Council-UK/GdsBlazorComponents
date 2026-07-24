e5e4d18f2c8f0453bea0800beded8def23a9e138
MattDorset <matthew.vincent@dorsetcouncil.gov.uk>
2026-07-24 17:33:48 +0100

    new checkbox item component

---
+++ GdsBlazorComponents/GdsCheckboxItem.razor.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace GdsBlazorComponents;
+
+public partial class GdsCheckboxItem
+{
+    [Parameter]
+    public RenderFragment? ChildContent { get; set; }
+
+    [Parameter]
+    public string? AdditionalCssClasses { get; set; }
+
+    [Parameter(CaptureUnmatchedValues = true)]
+    public IReadOnlyDictionary<string, object>? AdditionalAttributes { get; set; }
+
+    private FieldContext FieldContext = default!;
+    private string? _class;
+
+    protected override void OnInitialized()
+    {
+        FieldContext = new FieldContext(StateHasChanged);
+    }
+
+    protected override void OnParametersSet()
+    {
+        _class = new CssClassBuilder("govuk-checkboxes__item")
+            .Add(AdditionalCssClasses)
+            .Build();
+    }
+}
